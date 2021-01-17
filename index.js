f90704c44b871e128349e336489325e190fb5672
zach6585 <zmines@wesleyan.edu>
2021-01-17 17:46:51 -0500

    commit tree 0845318b2a29963ee79b4d762458f2ac7389f3c2

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+function saturdayFun(a = "roller-skate") {
+    return `This Saturday, I want to ${a}!`;
+}
+
+function mondayWork(a = "go to the office") {
+    return `This Monday, I will ${a}.`;
+}
+
+function wrapAdjective (str = "*") {
+    return function(statemnt = "special") {
+        return `You are ${str}${statemnt}${str}!`;
+};
+}
+
+let Calculator = {
+    num1 : 1, 
+    num2 : 3,
+    num3 : 10, 
+    num4 : 5,
+    add : function() {
+        return this["num1"] + this["num2"];
+    },
+    subtract : function() {
+        return this["num1"] - this["num2"];
+    },
+    multiply : function() {
+        return this["num1"] * this["num2"];
+    },
+    divide : function() {
+        return this["num3"] / this["num4"];
+    },
+} 
+
+function actionApplyer (starter, funarray) {
+    let j = starter 
+    for (const i of funarray) {
+        j = i(j) 
+    }
+    return j
+}
